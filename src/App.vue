9c539ea42312fdbfce3be271de99177b250da968
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-11-28 18:49:52 +0100

    Created First Component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Home />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Home from './components/Home'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Home
   }
 }
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Hello From Home View</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Home'
+}
+</script>
