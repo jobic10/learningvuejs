c173046c92210d0969e8f33fa8e0826daa994135
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-11-29 11:29:21 +0100

    Methods

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,6 +2,7 @@
     <div>
         <h1>Hello From Home View</h1>
         <h4>{{ data }}</h4>
+        <button v-on:click="hello()">Call</button>
     </div>
 </template>
 <script>
@@ -9,6 +10,11 @@ export default {
     name: 'Home',
     props:{
         data:String
+    },
+    methods: {
+        hello(){
+            alert("Hello Function Called")
+        }
     }
 }
 </script>
