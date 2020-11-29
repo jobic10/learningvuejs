ed5d854fb7dcb224a73d57b20fde8178d801293b
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-11-29 16:08:57 +0100

    For-loops

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,6 +6,18 @@
         <h1 v-if="show">True</h1>
         <h1 v-else> False</h1>
 
+<table border="1">
+<tr>
+<th>ID</th>
+<th>Name</th>
+<th>Email</th>
+</tr>
+<tr v-for="user in users" :key="user.email">
+<td>{{ user.id }}</td>
+<td>{{ user.name }}</td>
+<td>{{ user.email }}</td>
+</tr>
+</table>   
     </div>
 </template>
 <script>
@@ -21,7 +33,14 @@ export default {
     },
     data(){
         return{
-            show:false
+            show:false,
+            users:[
+                {id:1,name:"Owonubi Job",email:'jobowonubi@gmail.com'},
+                {id:2,name:"Ibrahim Faruq",email:'faruk@gmail.com'},
+                {id:3,name:"Akinola Roseline",email:'rose@gmail.com'},
+                {id:4,name:"Owonubi Solomon",email:'owonubi@gmail.com'},
+                {id:5,name:"Akinola Peter",email:'akinola@gmail.com'},
+                ]
         }
     }
 }
