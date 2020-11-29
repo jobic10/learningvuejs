efc4470658d882124c1b03d34617a22d7c51e109
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-11-29 11:31:28 +0100

    Passing data to function

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>Hello From Home View</h1>
         <h4>{{ data }}</h4>
-        <button v-on:click="hello()">Call</button>
+        <button v-on:click="hello('Job')">Call</button>
     </div>
 </template>
 <script>
@@ -12,8 +12,8 @@ export default {
         data:String
     },
     methods: {
-        hello(){
-            alert("Hello Function Called")
+        hello(name){
+            alert("Hello " + name)
         }
     }
 }
