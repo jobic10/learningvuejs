6014508639f1f95d56f9457d41d8edcd13bec23a
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-12-03 17:20:30 +0100

    Send props child to parent

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
 <div class="body">
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <Home data="We are thrilled to have you visit us"/>
-  <bind/>
-  <Users/>
-  <products></products>
+  <!-- <Home data="We are thrilled to have you visit us"/> -->
+  <child v-on:change-title="updateText($event)" />
+  <!-- <bind/> -->
+  <!-- <Users/> -->
+  <!-- <products></products> -->
   <h1 v-once>{{ name }}</h1>
   <h1>{{ name }}</h1>
   <button v-on:click="func()">Click</button>
@@ -13,27 +14,32 @@
 </template>
 
 <script>
-import Home from './components/Home'
-import Users from '@/components/Users'
-import Products from './components/Products.vue'
-import Bind from '@/components/Bind'
+// import Home from './components/Home'
+// import Users from '@/components/Users'
+// import Products from './components/Products.vue'
+// import Bind from '@/components/Bind'
+import Child from '@/components/Child'
 export default {
   name: 'App',
   components: {
-    Home,
-    Users,
-    Products,
-    Bind
+    // Home,
+    // Users,
+    // Products,
+    // Bind,
+    Child
   },
   data() {
     return {
-      name: "Job",
+      name: "Child to parent Data",
       'isValid': false
     }
   },
   methods: {
     func(){
       this.name = "Owonubi Job Sunday"
+    },
+    updateText(title){
+    this.name = title
     }
   },
 }
---
+++ src/components/Child.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <button v-on:click="updateTitle()">Update title</button>
+    </div>
+</template>
+<script>
+export default {
+    name: "Child",
+    methods: {
+        updateTitle(){
+            this.$emit('change-title', 'Child Props Has Been Passed!')
+        }
+    }
+}
+</script>
